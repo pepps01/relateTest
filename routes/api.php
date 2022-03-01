e248b9d85ba9d7404e5d4ed216425fae91008aae
pepps01 <slpepple01@gmail.com>
2022-03-01 11:00:23 +0100

    Initiate

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('user', function(){
+    return response()->json({
+        'message'=>"welcome"
+    })
+});
